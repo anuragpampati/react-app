63b56b3dc3ca0a551576408f8e72885fd87064d3
anuragpampati <anuragpampati696@gmail.com>
2024-02-11 02:47:11 -0500

    updated Listgroup to activate selected item

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,7 +1,11 @@
+import * as React from "react";
+
 function ListGroup() {
   let items = ["paris", "NY", "SF", "AL", "FL"];
   //   items = [];
-  const m = items.length === 0 ? <p>No items left</p> : null;
+  const m = items.length === 0 && <p>No items left</p>;
+  const [se, setSelectedIndex] = React.useState(-1);
+
   return (
     <>
       <h1>List</h1>
@@ -9,9 +13,13 @@ function ListGroup() {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              se === index ? "list-group-item active" : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log(item, index)}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
