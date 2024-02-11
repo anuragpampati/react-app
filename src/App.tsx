2e41cc98c9bd2e4bf8329b31b4f35c8af43c4a1a
anuragpampati <anuragpampati696@gmail.com>
2024-02-11 04:02:32 -0500

    included props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import ListGroup from "./Components/ListGroup";
 
-
-function App(){
-
-  return (<div>
-    <ListGroup />
-    </div>);
+function App() {
+  let items = ["paris", "NY", "SF", "AL", "FL"];
+  return (
+    <div>
+      <ListGroup items={items} heading={"cities"} />
+    </div>
+  );
 }
 
 export default App;
-
--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 
-function ListGroup() {
-  let items = ["paris", "NY", "SF", "AL", "FL"];
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
   //   items = [];
   const m = items.length === 0 && <p>No items left</p>;
   const [se, setSelectedIndex] = React.useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {m}
       <ul className="list-group">
         {items.map((item, index) => (
