09404e3ab2f278581d137353ef65ec8d2a749142
anuragpampati <anuragpampati696@gmail.com>
2024-03-09 18:22:48 -0500

    implemented filert

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import ExpenseFilter from "./expense-tracker/Components/ExpenseFilter";
 import ExpenseList from "./expense-tracker/Components/ExpenseList";
 
 function App() {
+  const [selectedCategory, setselectedCategory] = useState("");
   const [expense, setExpense] = useState([
     {
       id: 1,
@@ -22,11 +24,20 @@ function App() {
       category: "Utilities",
     },
   ]);
+  const visibleExpenses = selectedCategory
+    ? expense.filter((e) => e.category === selectedCategory)
+    : expense;
   if (expense.length == 0) return null;
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setselectedCategory(category)}
+        />
+      </div>
+
       <ExpenseList
-        expense={expense}
+        expense={visibleExpenses}
         onDelete={(id) => setExpense(expense.filter((e) => e.id != id))}
       />
     </div>
